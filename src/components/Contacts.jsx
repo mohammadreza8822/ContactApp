e862b25c1aadc4f9875b97ac627c6240a7b7688d
Mohammadreza <ma3968312@gmail.com>
2024-11-18 15:58:12 +0330

    set alert and add uuid for id

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 
 import ContactsList from "./ContactsList";
+import inputs from "../constants/inputs";
+import { v4 } from "uuid";
 
 function Contacts() {
   const [contacts, setContacts] = useState([]);
+  const [alert, setAlert] = useState("");
   const [contact, setContact] = useState({
+    id: "",
     name: "",
     lastName: "",
     email: "",
@@ -19,7 +23,18 @@ function Contacts() {
   };
 
   const addHandler = () => {
-    setContacts((contacts) => [...contacts, contact]);
+    if (
+      !contact.name ||
+      !contact.lastName ||
+      !contact.email ||
+      !contact.phone
+    ) {
+      setAlert("Please enter valid data!");
+      return;
+    }
+    setAlert("");
+    const newContact = { ...contact, id: v4() };
+    setContacts((contacts) => [...contacts, newContact]);
     setContact({
       name: "",
       lastName: "",
@@ -31,36 +46,19 @@ function Contacts() {
   return (
     <div>
       <div>
-        <input
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={contact.name}
-          onChange={changeHandler}
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          value={contact.lastName}
-          onChange={changeHandler}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={contact.email}
-          onChange={changeHandler}
-        />
-        <input
-          type="number"
-          placeholder="Phone"
-          name="phone"
-          value={contact.phone}
-          onChange={changeHandler}
-        />
+        {inputs.map((input, index) => (
+          <input
+            key={index}
+            type={input.type}
+            name={input.name}
+            placeholder={input.placeholder}
+            value={contact[input.name]}
+            onChange={changeHandler}
+          />
+        ))}
         <button onClick={addHandler}>Add Contact</button>
       </div>
+      <div>{alert && <p>{alert}</p>}</div>
       <ContactsList contacts={contacts} />
     </div>
   );
